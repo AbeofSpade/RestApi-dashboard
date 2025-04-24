655cfeeaf1ef50e34936c426f88a011d320fde34
AbeofSpade <R10NightWing@gmail.com>
2025-04-24 01:55:14 -0700

    updated a few files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
